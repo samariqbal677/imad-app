752da11e0f2e163a8084198e640fcc3a077ed60e
samariqbal677 <30714856+samariqbal677@users.noreply.github.com>
2017-08-07 19:55:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+app.get('/',function (req,res){
   res.sendFile(path.join(__dirname, 'ui', 'my first article.html'));
 });
 
