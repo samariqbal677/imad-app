95a2eaf44787ba30c1b8166536d65ee61b7f8b3f
samariqbal677 <30714856+samariqbal677@users.noreply.github.com>
2017-12-20 11:12:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,15 @@ app.get('/',function (req,res){
 });
 
 app.get('/article-one',function (req,res){
-  res.send('article-one requested and it will be served here');
+  res.send('article-one is requested and it will be served here');
+});
+
+app.get('/article-two',function (req,res){
+  res.send('article-two is requested and it will be served here');
+});
+
+app.get('/article-three',function (req,res){
+  res.send('article-three is requested and it will be served here');
 });
 
 app.get('/ui/style.css', function (req, res) {
