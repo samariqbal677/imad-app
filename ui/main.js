2ce1c432277a37c9658788727837beeef9c9fda4
samariqbal677 <30714856+samariqbal677@users.noreply.github.com>
2017-12-24 14:57:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,18 @@
 var button = document.getElementById('counter');
 
 button.onclick = function () {
-    var request = new XMLHttpRequest();
+
+        
+    };
+    request.open('GET','http://samariqbal677.imad.hasura-app.io/counter',true);
+    request.send(null);
+    
+};
+
+
+var submit = document.getElementById('submit_btn');
+submit.onclick =function(){
+        var request = new XMLHttpRequest();
     request.onreadystatechange = function()
     {
     if(request.readyState===XMLHttpRequest.DONE){
@@ -11,24 +22,13 @@ button.onclick = function () {
             span.innerHTML = counter.toString();
         }
     }
-        
-    };
-    request.open('GET','http://samariqbal677.imad.hasura-app.io/counter',true);
-    request.send(null);
-    
-};
-
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
-var submit = document.getElementById('submit_btn');
-submit.onclick =function(){
     var names =['name1','name2','name3','name4'];
     var list = '';
     for(var i=0;i<names.length;i++){
         list += '<li>' + names[i] + '</li>';
     }
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
     var ul = document.getElementById('namelist');
     ul.innerHTML = list ;
-    
-    
-};
+    };
