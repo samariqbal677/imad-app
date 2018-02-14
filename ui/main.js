bccbe6ab1c55c66619b618e81b9bd7a0ca139d3d
samariqbal677 <30714856+samariqbal677@users.noreply.github.com>
2018-02-15 01:33:21 +0530

    [imad-console] Updates ui/main.js

---
+++ ui/main.js
@@ -0,0 +1,37 @@
+var button = document.getElementById('counter');
+
+button.onclick = function () {
+
+        
+
+    
+};
+
+
+var submit = document.getElementById('submit_btn');
+submit.onclick =function(){
+        var request = new XMLHttpRequest();
+    request.onreadystatechange = function()
+    {
+    if(request.readyState===XMLHttpRequest.DONE){
+        if(request.status===200){
+            var counter =request.responseText;
+            var span =document.getElementById('count');
+            span.innerHTML = counter.toString();
+        }
+    }
+        };
+    request.open('GET','http://samariqbal677.imad.hasura-app.io/counter',true);
+    request.send(null);
+    
+    var names=request.responseText;
+    names=JSON.parse(names);
+    var list = '';
+    for(var i=0;i<names.length;i++){
+        list += '<li>' + names[i] + '</li>';
+    }
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list ;
+    };
