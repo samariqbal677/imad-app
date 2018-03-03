95eb002e8118a68a176942b5ee136d1f9fc82cfd
samariqbal677 <30714856+samariqbal677@users.noreply.github.com>
2018-03-03 13:48:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -102,7 +102,7 @@ app.get('/submit-name', function(req,res){
     res.send(JSON.stringify(names));
 });
 
-app.get('/article/:articleName',function (req,res){
+app.get('/articles/:articleName',function (req,res){
   pool.query("SELECT 8* FROM aricle WHERE title ='" + req.parms.articleName +"'" , function(err, result){
      if(err){
          res.status(500).send(err.toString());
