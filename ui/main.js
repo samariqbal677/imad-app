b1a10a3300da7c1e1350d96aa3a9babee45aaaef
samar iqbal <30714856+samariqbal677@users.noreply.github.com>
2018-04-05 09:51:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,8 @@
 function loadLoginForm () {
     var loginHtml = `
         <input type="file" onchange="previewFile()"><br>
-        <img src=""  alt="daisy" style = "width: 200px ;
-                      height: 200px ">
+        <img src="" "width": 200px ;
+                      "height": 200px >
         <h3>Please Register To Comment/Discuss On My WebPages</h3>
         <input type="text" id="username" placeholder="username" />
         <input type="password" id="password" placeholder="password" />
