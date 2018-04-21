c239166522bb326d0d3d51b9b603e191384df899
samar iqbal <30714856+samariqbal677@users.noreply.github.com>
2018-04-22 03:29:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -95,7 +95,7 @@ function loadLoginForm () {
 
 function previewFile(){
        var preview = document.querySelector('img');
-       var file    = document.querySelector('input[type=file]').files[0];
+       var file    = document.querySelector('input[type=file]').files[1];
        var reader  = new FileReader();
 
        reader.onloadend = function () {
